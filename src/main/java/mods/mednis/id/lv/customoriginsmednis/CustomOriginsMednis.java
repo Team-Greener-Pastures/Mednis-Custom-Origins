85f9c2f4e1a48da01de03a39297f70eae5edd268
Reinis Mednis <reinis@mednis.id.lv>
2022-04-09 21:51:13 +0300

    Disabled mixin

--- src/main/java/mods/mednis/id/lv/customoriginsmednis/CustomOriginsMednis.java
+++ src/main/java/mods/mednis/id/lv/customoriginsmednis/CustomOriginsMednis.java
@@ -1,19 +1,27 @@
 package mods.mednis.id.lv.customoriginsmednis;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CustomOriginsMednis implements ModInitializer {
 
     public static final Logger LOGGER = LoggerFactory.getLogger("Mednis - Origins");
-
+    public static final Item FABRIC_ITEM = new Item(new FabricItemSettings().group(ItemGroup.MISC));
 
     @Override
     public void onInitialize() {
 
         // Load all the powers!
         Powers.register();
+
+        // Register item for icon!
+        Registry.register(Registry.ITEM, new Identifier("customoriginsmednis", "fairy"), FABRIC_ITEM);
 
         LOGGER.info("-- Mednis Origins Loaded --");
         LOGGER.info("Currently adding: {} origin(s)",1);
