b25f23eb4a5cf8946a62ed1db4b9be60d9bce24b
Reinis Mednis <reinis@mednis.id.lv>
2022-08-07 21:22:17 +0300

    Ignore fairy fly speed changes when flying in spectator mode.
    Previously the fly speed for fairies would be reset even when flying in spectator mode. This change checks for it before doing any changes!

--- src/main/java/mods/mednis/id/lv/customoriginsmednis/powers/FlySpeedPower.java
+++ src/main/java/mods/mednis/id/lv/customoriginsmednis/powers/FlySpeedPower.java
@@ -12,11 +12,15 @@ public class FlySpeedPower extends Power {
 
         if (entity != null && entity.isPlayer()) {
             PlayerEntity player = (PlayerEntity) entity; // cast to player
+            if (!player.isSpectator()) { // Don't do any speed adjustments in spectator mode!
 
-            if (player.getAbilities().creativeMode) { // If we're in creative, don't add the speed change!
-                player.getAbilities().setFlySpeed(0.05F);
-            } else {
-                player.getAbilities().setFlySpeed(speed); // Change the player speed!
+                if (player.getAbilities().creativeMode) { // If we're in creative, don't add the speed change!
+
+                    player.getAbilities().setFlySpeed(0.05F); // Set it to a fast default speed!
+                } else {
+
+                    player.getAbilities().setFlySpeed(speed); // Change the player speed!
+                }
             }
         }
     }
